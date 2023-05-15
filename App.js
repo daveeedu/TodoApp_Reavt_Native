f0de92d3f5b2921a8e406bf10d6cf41e63596305
David Edu <daveeedu@gmail.com>
2023-05-16 00:25:03 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View, SafeAreaView, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/Header';
+import Task from './components/Task';
+import AddTask from './components/AddTask';
+import { v4 as uuidv4 } from 'uuid';
+import { getRandomBytes } from 'react-native-get-random-values';
 
 export default function App() {
+  const [tasks, setTasks] = useState([
+{"task":"HTML I","done":true, "id": "1"},
+{"task":"CSS","done":true, "id": "2"},
+{"task":"Responsive design","done":true, "id": "3"},
+{"task":"Git","done":true, "id": "4"},
+])
+
+const addTask = (text) => {
+  if (!text) {
+    Alert.alert('No Task', 'Please add a task', {text: "ok"})
+  } else {
+    setTasks(prevTasks => {
+      return [{task:text, id:uuidv4({ random: getRandomBytes })}, ...prevTasks]
+    })
+  }
+}
+
+const deleteTask = id => {
+  setTasks(prevTasks => {
+    return prevTasks.filter(task => task.id !== id)
+  })
+
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <AddTask addTask={addTask}/>
+        <View style={styles.list}>
+        <FlatList
+          data={tasks}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <Task item={item} deleteTask={deleteTask}/>
+         )}
+        />
+        </View>
+      </View>
+    </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -14,7 +57,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "black",
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  content: {
+    flex: 1,
+    padding: 30,
+  },
+  list: {
+    marginTop: 30,
+    flex: 1
+  },
+  textStyle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'black'
+  }
 });
---
+++ components/Task.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const Task = ({ item, deleteTask }) => {
+  return (
+    <TouchableOpacity style={styles.item}>
+        <Text >{item.task}</Text>
+        <MaterialIcons name="delete" size={24} color="black" onPress={() => deleteTask(item.id)}/>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        padding: 15,
+        borderColor: 'black',
+        borderRadius: 5,
+        borderWidth: 1,
+        marginTop: 15,
+        borderStyle: 'dotted',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+  });
+
+export default Task
